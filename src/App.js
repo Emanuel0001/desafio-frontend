5494864cee1b358b2ebc9358bdcf6e3434a1b893
emanuel <hitallosoares1@gmail.com>
2023-04-17 13:12:15 -0300

    correção erros/bugs no css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import iphone from './imagens/mobile.png'
 import selo from './imagens/seloSegurança.png'
 import garotoPropaganda from './imagens/coPrecato.png'
 import client from './imagens/client.png'
+import client2 from './imagens/client2.jpeg'
+import client3 from './imagens/client3.jpeg'
+import client4 from './imagens/client4.jpeg'
+
 import tamplateYoutube from './imagens/youtube.jpg'
 import { AiFillInstagram } from 'react-icons/ai';
 import { AiFillLinkedin } from 'react-icons/ai';
@@ -71,7 +75,7 @@ function App() {
         <div className='components'>
           <div className='component'>
             <div className='rectangle' id="rectangle">
-              <img className='image-rectangle' src={client}></img>
+              <img className='image-rectangle' src={client2}></img>
               <h1>Albino Noleto dos Santos</h1>
               <h2><ImLocation2 color='#24976B' size={20}/>Valparaíso de Goiás/GO</h2>
               <p>Fiz uma negociação com eles do precatório da minha aposentadoria especial. Enviei meus documentos, deu tudo certo e recebi os valores combinados. Fui muito bem atendido e a empresa cumpriu as suas obrigações. Todos os meus problemas foram resolvidos! Recomendo sem medo!” </p>
@@ -80,10 +84,10 @@ function App() {
           </div>
           <div className='component'>
             <div className='rectangle'>
-            <img className='image-rectangle' src={client}></img>
-              <h1>Albino Noleto dos Santos</h1>
-              <h2><ImLocation2 color='#24976B' size={20}/> Valparaíso de Goiás/GO</h2>
-              <p>Fiz uma negociação com eles do precatório da minha aposentadoria especial. Enviei meus documentos, deu tudo certo e recebi os valores combinados. Fui muito bem atendido e a empresa cumpriu as suas obrigações. Todos os meus problemas foram resolvidos! Recomendo sem medo!” </p>
+            <img className='image-rectangle' src={client3}></img>
+              <h1>AIris Gomes de Barros</h1>
+              <h2><ImLocation2 color='#24976B' size={20}/> Botucatu/SP</h2>
+              <p>A empresa fez um trabalho de aquisição de precatórios pertencentes à minha mãe com resultado totalmente satisfatório. Os responsáveis demonstraram sempre muita competência, responsabilidade e ética. Eu e minha família, em nome da minha mãe, damos testemunhos de se tratar” </p>
               <div className='polygon'></div>
             </div>
           </div>
@@ -92,18 +96,18 @@ function App() {
           <div className='component'>
             <div className='rectangle'>
             <img className='image-rectangle' src={client}></img>
-              <h1>Albino Noleto dos Santos</h1>
-              <h2><ImLocation2 color='#24976B' size={20}/> Valparaíso de Goiás/GO</h2>
-              <p>Fiz uma negociação com eles do precatório da minha aposentadoria especial. Enviei meus documentos, deu tudo certo e recebi os valores combinados. Fui muito bem atendido e a empresa cumpriu as suas obrigações. Todos os meus problemas foram resolvidos! Recomendo sem medo!” </p>
+              <h1>Wellington Silvero</h1>
+              <h2><ImLocation2 color='#24976B' size={20}/> Cotriguaçu/MT</h2>
+              <p>Primeiramente, gostaria de parabenizar a profissional que me atendeu por toda a sua atenção e prestatividade. Agradeço à empresa por sua idoneidade e compromisso com seus clientes. Deixo aqui meu agradecimento pelo excelente serviço prestado.” </p>
               <div className='polygon'></div>
             </div>
           </div>
           <div className='component'>
             <div className='rectangle'>
-            <img className='image-rectangle' src={client}></img>
-              <h1>Albino Noleto dos Santos</h1>
-              <h2><ImLocation2 color='#24976B' size={20}/> Valparaíso de Goiás/GO</h2>
-              <p>Fiz uma negociação com eles do precatório da minha aposentadoria especial. Enviei meus documentos, deu tudo certo e recebi os valores combinados. Fui muito bem atendido e a empresa cumpriu as suas obrigações. Todos os meus problemas foram resolvidos! Recomendo sem medo!” </p>
+            <img className='image-rectangle' src={client4}></img>
+              <h1>Maria Do Carmo</h1>
+              <h2><ImLocation2 color='#24976B' size={20}/> Comodoro/MT </h2>
+              <p>Muito obrigada pelo que fizeram por mim. No início, achei que era um trote. Graças a Deus não era! Com o dinheiro que caiu na minha conta, comprei um carro e estou reformando a minha casa. Foi muito bom negociar com vocês! “ </p>
               <div className='polygon'></div>
             </div>
           </div>
@@ -119,29 +123,30 @@ function App() {
               que é um aviso a União, Município ou Estado de que eles têm</p>
           </div>
           <div className='perguntas'>
-            <h1>Afinal, o que é esse tal de precatório?</h1>
-            <p>Quando a Fazenda Pública reconhece uma dívida com uma empresa ou pessoa, chamamos esse reconhecimento
-              de precatório. Após um processo, que costuma ser longo, o Poder Judiciário emite uma requisição de pagamento,
-              que é um aviso a União, Município ou Estado de que eles têm</p>
+            <h1>O que é um ofício requisitório?</h1>
+            <p>Ofício requisitório é o documento expedido depois que a pessoa ganha o processo. É esse documento que confirma a dívida da União, Estado e Município com uma pessoa ou uma empresa.</p>
           </div>
           <div className='perguntas'>
-            <h1>Afinal, o que é esse tal de precatório?</h1>
-            <p>Quando a Fazenda Pública reconhece uma dívida com uma empresa ou pessoa, chamamos esse reconhecimento
-              de precatório. Após um processo, que costuma ser longo, o Poder Judiciário emite uma requisição de pagamento,
-              que é um aviso a União, Município ou Estado de que eles têm</p>
+            <h1>Se eu negociar, como fica meu advogado?</h1>
+            <p>O seu advogado é parte essencial, é ele quem faz todo acompanhamento do andamento do processo e seus honorários contratuais estão garantidos. Durante a negociação os valores dele serão informados de forma clara, e se ele quiser também pode antecipar.</p>
           </div>
           <div className='perguntas'>
-            <h1>Afinal, o que é esse tal de precatório?</h1>
-            <p>Quando a Fazenda Pública reconhece uma dívida com uma empresa ou pessoa, chamamos esse reconhecimento
-              de precatório. Após um processo, que costuma ser longo, o Poder Judiciário emite uma requisição de pagamento,
-              que é um aviso a União, Município ou Estado de que eles têm</p>
+            <h1>Quais são os tipos de precatório?</h1>
+            <p>Existem dois tipos de precatório, os alimentares e os comuns. Precatório alimentar é aquele que decorre de salários, vencimentos, proventos, pensões e suas complementações, benefícios previdenciários e indenizações por morte ou por invalidez, fundadas em responsabilidade civil, em virtude de sentença judicial transitada em julgado. Já precatórios de origem comum são todos os...</p>
           </div>
           <div className='perguntas'>
-            <h1>Afinal, o que é esse tal de precatório?</h1>
-            <p>Quando a Fazenda Pública reconhece uma dívida com uma empresa ou pessoa, chamamos esse reconhecimento
-              de precatório. Após um processo, que costuma ser longo, o Poder Judiciário emite uma requisição de pagamento,
-              que é um aviso a União, Município ou Estado de que eles têm</p>
+            <h1>Eu posso negociar o meu precatório?</h1>
+            <p>Pode sim. Conforme o art. 100, §§ 13 e 14 da Constituição Federal de 1988 a negociação de precatórios está autorizada através da cessão de créditos, que é o nome usado para tratar a compra e venda de precatórios.</p>
           </div>
+          <div className='perguntas'>
+            <h1>Eu posso negociar o meu precatório?</h1>
+            <p>Pode sim. Conforme o art. 100, §§ 13 e 14 da Constituição Federal de 1988 a negociação de precatórios está autorizada através da cessão de créditos, que é o nome usado para tratar a compra e venda de precatórios.</p>
+          </div>
+          <div className='perguntas'>
+            <h1>Quais são os tipos de precatório?</h1>
+            <p>Existem dois tipos de precatório, os alimentares e os comuns. Precatório alimentar é aquele que decorre de salários, vencimentos, proventos, pensões e suas complementações, benefícios previdenciários e indenizações por morte ou por invalidez, fundadas em responsabilidade civil, em virtude de sentença judicial transitada em julgado. Já precatórios de origem comum são todos os demais casos, como, por exemplo, desapropriações, repetição de tributos, indenizações por danos morais, entre outros.</p>
+          </div>
+          
         </div>
       </div>
       <div className='container-title2'><h1>Maiores especialistas<br></br> do Brasil!</h1>
